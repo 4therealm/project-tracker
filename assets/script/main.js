b75875c5862a027a3bc16e0be7ff5e928a31f034
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-09 19:14:21 -0600

    pulled text from form name

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,7 +1,7 @@
 $( function() {
   $( "#datepicker" ).datepicker();
 
-
+let projectData = [];
 //element targeting 
 //form
 const formNameInput = $('#floatingInput')
@@ -18,13 +18,25 @@ const tableRows = Array.from(tableElement.children())
 
 
 
-let projectData = [
+
+
+$(formSubmit).on('click', $('button'), function(e) {
+  e.preventDefault()
+console.log("fired")
+const _name = $(formNameInput).text
+const _type = $(formTypeSelect).val()
+const _date = $(datePicker).input
+projectData = [
   {
-    name:formNameInput,
-    type:formTypeSelect,
-    due: datePicker
+    name:_name,
+    type:_type,
+    due: _date
   }
 ]
+console.log($(this).parent().children("input").val())
+// document.localStorage.setItem((formNameInput.text, projectData))
+  
+})
 
 
 
