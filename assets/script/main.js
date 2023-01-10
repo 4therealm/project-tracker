aacbcf0bd70c3f7e42776ff37125e3f9eea987a8
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-10 08:38:27 -0600

    render message works

--- assets/script/main.js
+++ assets/script/main.js
@@ -8,39 +8,49 @@ const formSubmit = $('#submitBtn')
 //table
 const tableElement = $('table')
 
-//table rows children will be stored as objects
-//th[0] = project name
-//th[1] = project type
-//th[2] = due date
-let tableData = [];
+let existingProjectData = JSON.parse(localStorage.getItem('existingProjectData'))
+if (existingProjectData ==null) existingProjectData = [];
+renderTable(existingProjectData)
 
-$('window').on("load", renderTable)
-
+//event listener-form submit
 $(formSubmit).on('click', $('button'), function(e) {
   e.preventDefault()
- //targeting input values
+
+  //targeting input values
   const _name = $(this).parent().children("input").val()
   const _type = $(this).parent().children("select").val()
   const _date = $(this).parent().children("p").children().val()
- 
+
   //object to hold input values
-  projectData = {name:_name,type:_type,dueDate: _date}
-     localStorage.setItem(('tableData'), JSON.stringify(projectData));
-  tableData.push(projectData)
-   //saving object, as a string, to local storag
-  
+  let newProjectData = {name:_name,type:_type,dueDate:_date}
+
+//saving object, as a string, to local storage
+  localStorage.setItem('newProjectData', JSON.stringify(newProjectData))
+      existingProjectData.push(newProjectData);
+  localStorage.setItem('existingProjectData', JSON.stringify(existingProjectData))
 })
-function renderTable() {
+
+
+
+
+
+
+
+
+
+
+
+function renderTable(array) {
  console.log('fired')
- tableData = tableData.concat(JSON.parse(localStorage.getItem('tableData')||'[]'))
-tableData.forEach(obj => {
+
+array.forEach(obj => {
 
     var row =  $('<tr/>'); 
     row.append($('<td/>').append(obj.name));
     row.append($('<td/>').append(obj.type));
     row.append($('<td/>').append(obj.dueDate).attr('scope', 'row'));
     tableElement.find('tbody').append(row);
-    console.log(obj)
+    console.log(array)
 })}
   
 
