68de20521d3516f66bedefc7c5e231651a4c3d03
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-09 20:28:27 -0600

    validator on form inputs

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,7 +1,7 @@
 $( function() {
   $( "#datepicker" ).datepicker();
 
-let projectData = [];
+let projectDataMaster = [];
 //element targeting 
 //form
 const formNameInput = $('#floatingInput')
@@ -16,27 +16,42 @@ const tableRows = Array.from(tableElement.children())
 //th[1] = project type
 //th[2] = due date
 
-
+let inputValidator = {
+  projectName: false,
+  type: false,
+  dueDate: false
+}
 
 
 
 $(formSubmit).on('click', $('button'), function(e) {
   e.preventDefault()
-console.log("fired")
-const _name = $(this).parent().children("input").val()
-const _type = $(this).parent().children("select").val()
-const _date = $(this).parent().children("p").children().val()
+ //targeting input values
+  const _name = $(this).parent().children("input").val()
+  const _type = $(this).parent().children("select").val()
+  const _date = $(this).parent().children("p").children().val()
+ if (_name.length > 0) {
+  inputValidator[projectName] = true;
+} else {
+  inputValidator[projectName] = false;
+ }
+ let allTrue = Object.keys(inputValidator).every((item) => {
+  return inputValidator[item] === true
+});
 
+if (allTrue) {
+  formSubmit.disabled = false;
+} else {
+  formSubmit.disabled = true;
+}
+})
+})
 
-projectData = [
-  {
-    name:_name,
-    type:_type,
-    due: _date
-  }
-]
-console.log(projectData)
-localStorage.setItem(_name, JSON.stringify(projectData));
+  
+  //object to hold input values
+  projectData = [{name:_name,type:_type,dueDate: _date}]
+   //saving object, as a string, to local storage
+  localStorage.setItem(_name, JSON.stringify(projectData));
   
 })
 
