74431b4963906566a133424298bd5aa6479c615f
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-09 18:41:27 -0600

    getting js structure and targeting compelete

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,13 +2,29 @@ $( function() {
   $( "#datepicker" ).datepicker();
 
 
+//element targeting 
+//form
+const formNameInput = $('#floatingInput')
+const formTypeSelect = $('#floatingSelect')
+const datePicker = $('#datePicker')
+const formSubmit = $('#submitBtn')
+//table
+const tableElement = $('table')
+const tableRows = Array.from(tableElement.children())
+//table rows children will be stored as objects
+//th[0] = project name
+//th[1] = project type
+//th[2] = due date
 
 
 
-
-
-
-
+let projectData = [
+  {
+    name:formNameInput,
+    type:formTypeSelect,
+    due: datePicker
+  }
+]
 
 
 
